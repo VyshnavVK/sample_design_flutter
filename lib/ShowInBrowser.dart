a364c37dabe4432824cb9f4d2fce33a7b33bef0c
vyshnavvk <developer39>
2022-06-29 18:23:48 +0530

    commit

--- lib/ShowInBrowser.dart
+++ lib/ShowInBrowser.dart
@@ -1,47 +1,54 @@
 import 'dart:async';
-import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ShowInBrowser extends StatefulWidget {
- final String? url;
- final BuildContext context;
+  final String? url;
+  final BuildContext context;
 
- ShowInBrowser(this.context,this.url);
+  ShowInBrowser(this.context, this.url);
 
   @override
   State<ShowInBrowser> createState() => _ShowInBrowserState();
 }
 
 class _ShowInBrowserState extends State<ShowInBrowser> {
- late WebViewController _webViewController;
+  late WebViewController _webViewController;
 
- final Completer<WebViewController> _controller =  Completer<WebViewController>();
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
 
- String currentUrl = "";
+  String currentUrl = "";
 
   @override
   Widget build(BuildContext context) {
     currentUrl = widget.url!;
-    return WillPopScope( onWillPop: () async {
-      return true;
-    },
-      child: Scaffold(appBar: AppBar(title: Text(currentUrl)),body: WebView(
-      initialUrl: widget.url!,
-      javascriptMode: JavascriptMode.unrestricted,
-      onWebViewCreated: (WebViewController webViewController) {
-        _webViewController = webViewController;
-        _controller.complete(webViewController);
+    return WillPopScope(
+      onWillPop: () async {
+        if (await _webViewController.canGoBack()) {
+          _webViewController.goBack();
+          return false;
+        }
+        return true;
       },
-      onPageFinished:(finish){
-        setState(() {
-          currentUrl = _webViewController.currentUrl().toString();
-        });
-      },
-    ),),);
-
-
+      child: Scaffold(
+        appBar: AppBar(title: Text(currentUrl)),
+        body: WebView(
+          initialUrl: widget.url!,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+            _webViewController = webViewController;
+          },
+          onPageFinished: (finish) {
+            setState(() async {
+              currentUrl = (await _webViewController.currentUrl())!;
+              AppBar(title: Text(currentUrl));
+            });
+          },
+        ),
+      ),
+    );
   }
 }
-
